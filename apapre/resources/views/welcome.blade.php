41cedfed1640e453972ac9abda9f046b444fe567
VitorLuckmann <vitorlk.trabalho@gmail.com>
2024-06-28 21:54:23 -0300

    Update welcome.blade.php

--- apapre/resources/views/welcome.blade.php
+++ apapre/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel teste vitor Otavi zéca</title>
+        <title>Laravel teste vitor Otadenfnwevi zéca</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
